af408004613c3321dc7f86b1ae3d52eb3805c40b
replygirl <moon@moon.fyi>
2020-11-20 14:10:17 -0500

    hello

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+span idk probably but maybe only the good ones will be left at the end
+</template>
+
+<style lang="stylus">
+@font-face
+  font-family 'Harriet Text'
+  font-style italic
+  font-weight bold
+  src local('Harriet Text Bold'), url('/src/assets/HARRIETTEXTBOLDITALIC.woff') format('woff')
+
+:root
+  background-color #fff200
+  font normal italic bold 34px/40px 'Harriet Text', serif
+  &, body, #app
+    height 100%
+
+#app
+  align-items center
+  display flex
+  justify-content center
+  margin 0 auto
+  max-width 640px
+  padding 40px 40px
+
+span
+  text-align center
+</style>
